a82adf98b52df76004cac88e595e2994ce33b5e9
Francisco Guzmán <paco.mgh@gmail.com>
2020-06-22 18:31:01 -0500

    add external class wordpair and infinite listview

--- lib/main.dart
+++ lib/main.dart
@@ -6,18 +6,80 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
+    //final wordPair = new WordPair.random();
     return MaterialApp(
       title: 'Welcome to Flutter',
-      home: Scaffold(
+      home: RandomWord()
+      /*Scaffold(
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
         body: Center(
           //child: const Text('Hello World'),
-          child: Text(wordPair.asPascalCase),
+          //child: Text(wordPair.asPascalCase),
+          child: RandomWord(),
         ),
+      ),*/
+    );
+  }
+}
+
+class RandomWord extends StatefulWidget {
+  @override
+  _RandomWordState createState() => _RandomWordState();
+}
+
+class _RandomWordState extends State<RandomWord> {
+  final List<WordPair> _suggestion = <WordPair>[];
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
+  @override
+  Widget build(BuildContext context) {
+    //final wordPair = WordPair.random();
+    //return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Startup name gennerator"),
+      ),
+      body: _buildSuggestion(),
+    );
+  }
+
+  Widget _buildSuggestion(){
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        // The itemBuilder callback is called once per suggested
+        // word pairing, and places each suggestion into a ListTile
+        // row. For even rows, the function adds a ListTile row for
+        // the word pairing. For odd rows, the function adds a
+        // Divider widget to visually separate the entries. Note that
+        // the divider may be difficult to see on smaller devices.
+        itemBuilder: (BuildContext _context, int i){
+          // Add a one-pixel-high divider widget before each row
+          // in the ListView.
+          if(i.isOdd)
+            return Divider();
+          // The syntax "i ~/ 2" divides i by 2 and returns an
+          // integer result.
+          // For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
+          // This calculates the actual number of word pairings
+          // in the ListView,minus the divider widgets.
+          final int index = i ~/ 2;
+          // If you've reached the end of the available word
+          // pairings...
+          if(index>= _suggestion.length)
+            _suggestion.addAll(generateWordPairs().take(10));
+          return _buildRow(_suggestion[index]);
+        }
+    );
+  }
+
+  Widget _buildRow(WordPair pair){
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
       ),
     );
   }
+
 }
